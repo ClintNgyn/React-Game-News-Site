b7f2a5f6496ee70905e10a955173ada7fc837490
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-01-20 11:54:36 -0500

    redux setup

---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import gamesReducer from './gamesReducer';
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  games: gamesReducer,
+});
+
+export default rootReducer;
